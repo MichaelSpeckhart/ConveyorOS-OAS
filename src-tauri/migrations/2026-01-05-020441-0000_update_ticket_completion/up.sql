fd6765be59dfe905bb048fc95a391687c3bdafec
MichaelSpeckhart <msspeck394@gmail.com>
2026-02-19 10:16:34 -0500

    New Migrations

--- src-tauri/migrations/2026-01-05-020441-0000_update_ticket_completion/up.sql
+++ src-tauri/migrations/2026-01-05-020441-0000_update_ticket_completion/up.sql
@@ -1,3 +1,4 @@
 -- Your SQL goes here
 -- ALTER TABLE tickets
 -- ADD COLUMN garments_processed INTEGER NOT NULL DEFAULT 0;
+SELECT 1;
--- src-tauri/migrations/2026-02-03-194333-0000_session_add_admin/down.sql
+++ src-tauri/migrations/2026-02-03-194333-0000_session_add_admin/down.sql
@@ -1,4 +1,6 @@
 -- This file should undo anything in `up.sql`
 -- This file should undo anything in `up.sql`
-ALTER TABLE sessions;
-DROP COLUMN is_admin;
+-- ALTER TABLE sessions;
+-- DROP COLUMN is_admin;
+
+select 1;
